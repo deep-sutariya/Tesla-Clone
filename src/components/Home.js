cf3c06cfe813c7af652084d525b31eb7feea6f0f
deep-sutariya <117342002+deep-sutariya@users.noreply.github.com>
2022-11-21 13:33:55 +0530

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,49 +9,49 @@ function Home() {
         <Section 
             title="Model 3"
             descripition="Order Online for Touchless Delivery"
-            backgroundImg="/images/model-3.jpg"
+            backgroundImg="./images/model-3.jpg"
             leftBtnText="Custom Order"
             rightBtnText="Existing Inventory"
         />
         <Section 
             title="Model Y"
             descripition="Order Online for Touchless Delivery"
-            backgroundImg="/images/model-y.jpg"
+            backgroundImg="./images/model-y.jpg"
             leftBtnText="Custom Order"
             rightBtnText="Existing Inventory"
         />
         <Section 
             title="Model S"
             descripition="Order Online for Touchless Delivery"
-            backgroundImg="/images/model-s.jpg"
+            backgroundImg="./images/model-s.jpg"
             leftBtnText="Custom Order"
             rightBtnText="Existing Inventory"
         />
         <Section 
             title="Model x"
             descripition="Order Online for Touchless Delivery"
-            backgroundImg="/images/model-x.jpg"
+            backgroundImg="./images/model-x.jpg"
             leftBtnText="Custom Order"
             rightBtnText="Existing Inventory"
         />
         <Section 
             title="Lowest Cost Solar Panels in America"
             descripition="Money-back guarantee"
-            backgroundImg="/images/solar-panel.jpg"
+            backgroundImg="./images/solar-panel.jpg"
             leftBtnText="Order Now"
             rightBtnText="Learn more"
         />
         <Section 
             title="Solar For New Roofs"
             descripition="Solar Roof Costs Less Than a New Roof Plus Solar Panels"
-            backgroundImg="/images/solar-roof.jpg"
+            backgroundImg="./images/solar-roof.jpg"
             leftBtnText="Order Now"
             rightBtnText="Learn more"
         />
         <Section 
             title="Accessories"
             descripition=""
-            backgroundImg="/images/accessories.jpg"
+            backgroundImg="./images/accessories.jpg"
             leftBtnText="Shop Now"
             // rightBtnText="Learn more"
         />
