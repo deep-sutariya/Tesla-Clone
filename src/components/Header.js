0e40e3f0ad9b382ef70b0d59b1a94270b53610bc
deep <d33psutariya@gmail.com>
2023-05-09 12:23:56 +0530

    Resolve Image Bug

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,8 @@ import CloseIcon from '@mui/icons-material/Close';
 import { selectCars } from '../features/car/carSlice';
 import { useSelector } from 'react-redux';
 import "./header.css";
-import { style } from '@mui/system';
+
+const Tesla_Logo = require('../assets/logo.png');
 
 
 function Header() {
@@ -16,57 +17,58 @@ function Header() {
     function remove() {
         document.getElementById('homeid').classList.remove('active');
     }
-    let headerKey=document.querySelector('header2');
-    const scr=(index)=>{
-        if(index===0) headerKey.scrollIntoView()//headerKey.classList.toggle(window.scrollTo({top:0,behavior:"smooth"}));
-        if(index===1) headerKey.classList.toggle(window.scrollTo({top:10,behavior:"smooth"}));
-        if(index===2) headerKey.classList.toggle(window.scrollTo({top:20,behavior:"smooth"}));
-        if(index===3) headerKey.classList.toggle(window.scrollTo({top:30,behavior:"smooth"}));
+    let headerKey = document.querySelector('header2');
+    const scr = (index) => {
+        if (index === 0) headerKey.scrollIntoView()//headerKey.classList.toggle(window.scrollTo({top:0,behavior:"smooth"}));
+        if (index === 1) headerKey.classList.toggle(window.scrollTo({ top: 10, behavior: "smooth" }));
+        if (index === 2) headerKey.classList.toggle(window.scrollTo({ top: 20, behavior: "smooth" }));
+        if (index === 3) headerKey.classList.toggle(window.scrollTo({ top: 30, behavior: "smooth" }));
     }
 
     return (
         <div className='hcontainer'>
 
-            <img src='/images/tesla-logo.svg' alt='' />
+            <img src={Tesla_Logo} />
+
 
 
             <div className='menu'>
 
                 {cars && cars.map((car, index) => (
-                    <li><a key={index} id='header2' href='#' onClick={()=>scr(index)}>{car}</a></li>
+                    <li><a key={index} id='header2' href='/' onClick={() => scr(index)}>{car}</a></li>
                 ))}
-                
+
             </div>
 
             <div className='right_menu'>
-                <a href='#'>Shop</a>
-                <a href='#'>Account</a>
-                <a href='#' id='open_menu' onClick={()=>{setburgerStat(true);toggle()}}>
+                <a href='/'>Shop</a>
+                <a href='/'>Account</a>
+                <a href='/' id='open_menu' onClick={() => { setburgerStat(true); toggle() }}>
                     Menu
                 </a>
             </div>
 
             <div className='burgur_nav' id='popup' style={{ transform: burgerStat ? 'translateX(0%)' : "translateX(100%)" }}>
                 <div className='close_icon'  >
-                    <div className='icon' onClick={() => {setburgerStat(false);remove()}}>
+                    <div className='icon' onClick={() => { setburgerStat(false); remove() }}>
                         <CloseIcon />
                     </div>
                 </div>
 
                 {cars && cars.map((car, index) => (
-                    <li key={index}><a href='#'>{car}</a></li>
+                    <li key={index}><a href='/'>{car}</a></li>
                 ))}
 
-                <li><a href='#'>Existing Inventory</a></li>
-                <li><a href='#'>Used Inventory</a></li>
-                <li><a href='#'>Trade-in</a></li>
-                <li><a href='#'>Cybertruck</a></li>
-                <li><a href='#'>Roadrester</a></li>
-                <li><a href='#'>Test Drive</a></li>
-                <li><a href='#'>Insurance</a></li>
-                <li><a href='#'>Powerwall</a></li>
-                <li><a href='#'>Support</a></li>
-                
+                <li><a href='/'>Existing Inventory</a></li>
+                <li><a href='/'>Used Inventory</a></li>
+                <li><a href='/'>Trade-in</a></li>
+                <li><a href='/'>Cybertruck</a></li>
+                <li><a href='/'>Roadrester</a></li>
+                <li><a href='/'>Test Drive</a></li>
+                <li><a href='/'>Insurance</a></li>
+                <li><a href='/'>Powerwall</a></li>
+                <li><a href='/'>Support</a></li>
+
             </div>
         </div>
     )
--- src/components/Home.js
+++ src/components/Home.js
@@ -2,6 +2,14 @@ import React from 'react';
 import Section from './Section';
 import "./home.css";
 
+const Model3 = require('../assets/model-3.jpg');
+const Modely = require('../assets/model-y.jpg');
+const Models = require('../assets/model-s.jpg');
+const Modelx = require('../assets/model-x.jpg');
+const Solar_Panel = require('../assets/solar-panel.jpg');
+const Solar_Roof = require('../assets/solar-roof.jpg');
+const Accessories = require('../assets/accessories.jpg');
+
 
 function Home() {
   return (
@@ -10,49 +18,49 @@ function Home() {
         <Section 
             title="Model 3"
             descripition="Order Online for Touchless Delivery"
-            backgroundImg={`${process.env.PUBLIC_URL}/images/model-3.jpg`}
+            backgroundImg={Model3}
             leftBtnText="Custom Order"
             rightBtnText="Existing Inventory"
         />
         <Section 
             title="Model Y"
             descripition="Order Online for Touchless Delivery"
-            backgroundImg={`${process.env.PUBLIC_URL}/images/model-y.jpg`}
+            backgroundImg={Modely}
             leftBtnText="Custom Order"
             rightBtnText="Existing Inventory"
         />
         <Section 
             title="Model S"
             descripition="Order Online for Touchless Delivery"
-            backgroundImg={`${process.env.PUBLIC_URL}/images/model-s.jpg`}
+            backgroundImg={Models}
             leftBtnText="Custom Order"
             rightBtnText="Existing Inventory"
         />
         <Section 
             title="Model x"
             descripition="Order Online for Touchless Delivery"
-            backgroundImg={`${process.env.PUBLIC_URL}/images/model-x.jpg`}
+            backgroundImg={Modelx}
             leftBtnText="Custom Order"
             rightBtnText="Existing Inventory"
         />
         <Section 
             title="Lowest Cost Solar Panels in America"
             descripition="Money-back guarantee"
-            backgroundImg={`${process.env.PUBLIC_URL}/images/solar-panel.jpg`}
+            backgroundImg={Solar_Panel}
             leftBtnText="Order Now"
             rightBtnText="Learn more"
         />
         <Section 
             title="Solar For New Roofs"
             descripition="Solar Roof Costs Less Than a New Roof Plus Solar Panels"
-            backgroundImg={`${process.env.PUBLIC_URL}/images/solar-roof.jpg`}
+            backgroundImg={Solar_Roof}
             leftBtnText="Order Now"
             rightBtnText="Learn more"
         />
         <Section 
             title="Accessories"
             descripition=""
-            backgroundImg={`${process.env.PUBLIC_URL}/images/accessories.jpg`}
+            backgroundImg={Accessories}
             leftBtnText="Shop Now"
             // rightBtnText="Learn more"
         />
